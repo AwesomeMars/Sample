599b39703b72f0dbbbc089b701f301812fbadf5f
ser u <user@el-capitan-cs6-3.local>
2016-08-24 18:01:40 +0900

    상속을 통한 도형 세부 구현

--- Sample/main.m
+++ Sample/main.m
@@ -17,48 +17,105 @@
 @end
 
 @implementation Shape
+{
+    float x, y;
+    float w, h;
+    NSString *color;
+}
+-(id)init{
+    self = [super init];
+    if(self){
+        x=0;
+        y=0;
+        w=10;
+        h=10;
+        color = @"Black";
+    }
+    return self;
+    
+}
+-(void)setX:(float)newX withY:(float)newY
+{
+    x = newX;
+    y = newY;
+}
+- (void)setWidth:(float)newW withHeight:(float)newH
+{
+    w = newW;
+    h = newH;
+}
+-(void)setColor:(NSString*)newColor
+{
+    color = newColor;
+}
 -(void)draw
 {
     NSLog(@"공통 그리기 로직");
+    NSLog(@"X= %g, Y=%g, W=%g, H=%g", x, y, w, h);
+    NSLog(@"Color = %@", color);
 }
 @end
 
+
+
 //#########
 //Rectangle
-@interface Rectangle : NSObject
--(void)drawRectangle;
+@interface Rectangle : Shape
 @end
 
 @implementation Rectangle
--(void)drawRectangle
+-(void)draw
 {
-    NSLog(@"공통 그리기 로직");
-    NSLog(@"사각형 그리기");
+    [super draw];
+    NSLog(@"사각형 그리다.");
 }
 @end
 //########
 //Circle
-@interface Circle : NSObject
--(void)draw;
+@interface Circle : Shape
 @end
 
 @implementation Circle
--(void)draw;
+-(void)draw
 {
-    NSLog(@"공통 그리기 로직");
-    NSLog(@"원 그리기");
+    [super draw];    //원은 draw와 상관없어 !! 뺄 경우 공통 그리기 로직은 호출을 안함.
+    NSLog(@"원 그리다.");
 }
 @end
 
 
+//########
+//##Super Circle
+@interface SuperCircle : Circle
+@end
+@implementation SuperCircle
+-(void)draw
+{
+    [super draw];
+    NSLog(@"빤짝~ 빤짝");
+    
+}
+@end
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        Circle *circle = [[Circle alloc]init];
-        [circle draw];
-        
+        Circle *circle = [[Circle alloc]init];    // init은 맴버 변수의 초기화를 주로, 수동메모리 관리 시에는 dealloc이라는 메소드도 쓴다.
         Rectangle *rectangle = [[Rectangle alloc]init];
-        [rectangle drawRectangle];
+        SuperCircle *sCircle = [[SuperCircle alloc]init];
+        
+//        [circle setWidth:100 withHeight:100];
+//        [circle setX:100 withY:200];
+//        [circle setColor:@"RED"];
+        
+        [sCircle draw];
+        [rectangle setWidth:10 withHeight:10];
+        [rectangle setX:10 withY:20];
+        [rectangle setColor:@"BLUE"];
+        
+        [circle draw];
+        [rectangle draw];
+        
+        //다음시간, 동적 바인딩을 통해서!!
     }
     return 0;
 }
